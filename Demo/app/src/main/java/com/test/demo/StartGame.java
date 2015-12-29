b575b60668e7bea9cff6db3d6fca6d6b8c7051a2
陳韋丞 <jackie0607331008@gmail.com>
2015-12-29 22:01:03 +0800

    12/29 score and time and dialog finsh

--- Demo/app/src/main/java/com/test/demo/StartGame.java
+++ Demo/app/src/main/java/com/test/demo/StartGame.java
@@ -1,5 +1,8 @@
 package com.test.demo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +11,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.sql.Statement;
 
 
 public class StartGame extends AppCompatActivity implements View.OnClickListener{
@@ -15,7 +19,8 @@ public class StartGame extends AppCompatActivity implements View.OnClickListener
     TextView tvscore;
     TextView tvtime;
     int score=0;
-    int startTime=15;
+    int startTime=5;
+    AlertDialog.Builder builder;
     private Handler handler =new Handler();
 
     @Override
@@ -25,7 +30,7 @@ public class StartGame extends AppCompatActivity implements View.OnClickListener
         tvscore=(TextView)findViewById(R.id.textView2);
         handler.postDelayed(updataTimer,1000);
         image=new ImageView[9];
-
+        builder=new AlertDialog.Builder(this);
         for(int i=0;i<=8;i++){
             String name="imageView"+(i+1);
             image[i]=(ImageView)findViewById(getResources().getIdentifier(name,"id",getPackageName()));
@@ -57,11 +62,27 @@ public class StartGame extends AppCompatActivity implements View.OnClickListener
             }else{
                 for(int i=0;i<=8;i++){
                     String name="imageView"+(i+1);
-
                     image[i].setOnClickListener(null);
-
-
                 }
+                    builder.setTitle("時間到了!");
+                    builder.setMessage("恭喜你得到" + score + "分");
+                    builder.setPositiveButton("繼續遊戲", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            startActivity(new Intent(StartGame.this, StartGame.class));
+                        }
+                    });
+                    builder.setNegativeButton("結束遊戲", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            startActivity(new Intent(StartGame.this, MainActivity.class));
+                            finish();
+                        }
+                    });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+
             }
             startTime -= 1;
         }
