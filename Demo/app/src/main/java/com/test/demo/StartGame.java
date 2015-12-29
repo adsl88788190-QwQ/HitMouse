65528d15b1c69666e8650c97ca570add30ac1c0e
陳韋丞 <jackie0607331008@gmail.com>
2015-12-29 20:54:50 +0800

    12/29 score finsh

--- Demo/app/src/main/java/com/test/demo/StartGame.java
+++ Demo/app/src/main/java/com/test/demo/StartGame.java
@@ -2,12 +2,40 @@ package com.test.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class StartGame extends AppCompatActivity {
+public class StartGame extends AppCompatActivity implements View.OnClickListener{
+    ImageView[] image;
+    TextView tvscore;
+    int score=0;
 
+//    int time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
+        tvscore=(TextView)findViewById(R.id.textView2);
+        image=new ImageView[9];
+        for(int i=0;i<=8;i++){
+            String name="imageView"+(i+1);
+            image[i]=(ImageView)findViewById(getResources().getIdentifier(name,"id",getPackageName()));
+            image[i].setOnClickListener(this);
+
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String show;
+        ImageView newimage=(ImageView)v;
+        if(newimage.getDrawable().getCurrent().getConstantState()==getResources().getDrawable(R.drawable.boss).getConstantState())
+            score-=50;
+        else
+            score+=5;
+        tvscore.setText("score:"+score);
+
     }
 }
