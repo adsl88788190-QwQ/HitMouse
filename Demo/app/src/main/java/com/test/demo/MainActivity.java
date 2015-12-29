0d499567ab8ff18e4a6613d535f88e713a169905
陳韋丞 <jackie0607331008@gmail.com>
2015-12-29 14:42:38 +0800

    Second 12/29

--- Demo/app/src/main/java/com/test/demo/MainActivity.java
+++ Demo/app/src/main/java/com/test/demo/MainActivity.java
@@ -11,12 +11,39 @@ public class MainActivity extends AppCompatActivity {
     Button start;
     Button change;
     Button score;
+    Button share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start=(Button)findViewById(R.id.Start);
+        change=(Button)findViewById(R.id.Change);
+        score=(Button)findViewById(R.id.Score);
+        share=(Button)findViewById(R.id.primary_button);
 
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ChooseList.class));
+            }
+        });
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ChangeWeapon.class));
+            }
+        });
+        score.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ScoreList.class));
+            }
+        });
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
+            }
+        });
     }
 }
