5d0af2f3dc54baa998aa75aed04eb17d8651ff50
陳韋丞 <jackie0607331008@gmail.com>
2015-12-29 18:34:01 +0800

    12/29 apk

--- Demo/app/src/main/java/com/test/demo/ChooseList.java
+++ Demo/app/src/main/java/com/test/demo/ChooseList.java
@@ -1,13 +1,24 @@
 package com.test.demo;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ChooseList extends AppCompatActivity {
-
+    ImageView image2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chooseList);
+        setContentView(R.layout.activity_chooselist);
+        image2=(ImageView)findViewById(R.id.imageView2);
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ChooseList.this,StartGame.class));
+            }
+        });
     }
 }
